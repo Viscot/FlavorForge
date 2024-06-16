4aee7d877cfdcb8a01e1c287163a577e3a4dd7e3
Viscot <137077936+Viscot@users.noreply.github.com>
2024-06-17 00:34:57 +0700

    Update favorite page

--- lib/screen/favorite_page.dart
+++ lib/screen/favorite_page.dart
@@ -24,57 +24,86 @@ class Recipe {
     required this.steps,
     this.isFavorite = true,
   });
+
+  static Future<Recipe> fromFirestore(DocumentSnapshot doc) async {
+    Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+    DocumentReference authorRef = data['authorId'];
+    String creator = await _getUsernameFromUserRef(authorRef);
+
+    return Recipe(
+      id: doc.id,
+      title: data['title'] ?? '',
+      description: data['description'] ?? '',
+      imageUrl: data['imageUrl'] ?? '',
+      creator: creator,
+      ingredients: List<String>.from(data['ingredients'] ?? []),
+      steps: List<String>.from(data['steps'] ?? []),
+    );
+  }
+
+  static Future<String> _getUsernameFromUserRef(
+      DocumentReference userRef) async {
+    DocumentSnapshot userDoc = await userRef.get();
+    Map<String, dynamic> userData = userDoc.data() as Map<String, dynamic>;
+    return userData['username'] ?? 'Unknown';
+  }
 }
 
-class FavoritePage extends StatelessWidget {
-  final List<Recipe> favoriteRecipes = [
-    Recipe(
-      title: 'Gado gado Entong',
-      description: 'Gado-gado adalah salah satu makanan khas Indonesia.',
-      imageUrl: 'https://nibble-images.b-cdn.net/nibble/original_images/gado_gado_enak_di_jakarta_03_515ab09c9d.jpg',
-      creator: 'Entong ganteng',
-      ingredients: [
-        '100g lontong',
-        '50g kacang tanah',
-        '50g sayuran',
-        '3 telur rebus',
-        '350g saus kacang',
-        '2 cabai',
-        '50g kerupuk',
-      ],
-      steps: [
-        'Step 1: Siapkan bahan-bahan.',
-        'Step 2: Rebus sayuran dan telur.',
-        'Step 3: Campur semua bahan dengan saus kacang.',
-      ],
-    ),
-    Recipe(
-      title: 'Nasi Goreng Toyib',
-      description: 'Nasi Goreng adalah makanan khas Indonesia yang lezat.',
-      imageUrl: 'https://example.com/nasigoreng.jpg',
-      creator: 'Toyib cakep',
-      ingredients: [
-        '2 cups cooked rice',
-        '2 tablespoons oil',
-        '1 onion, chopped',
-        '2 cloves garlic, minced',
-        '100g chicken, diced',
-        '2 eggs, beaten',
-        '1 tablespoon soy sauce',
-        'Salt and pepper to taste',
-      ],
-      steps: [
-        'Step 1: Heat oil in a pan.',
-        'Step 2: Sauté onion and garlic.',
-        'Step 3: Add chicken and cook until done.',
-        'Step 4: Add cooked rice and stir well.',
-        'Step 5: Push rice to the side and scramble the eggs.',
-        'Step 6: Mix everything together and add soy sauce.',
-        'Step 7: Season with salt and pepper.',
-      ],
-    ),
-    // Tambahkan 8 resep lainnya dengan cara yang sama
-  ];
+class FavoritePage extends StatefulWidget {
+  @override
+  _FavoritePageState createState() => _FavoritePageState();
+}
+
+class _FavoritePageState extends State<FavoritePage> {
+  List<Recipe> favoriteRecipes = [];
+  bool isRefresh = false;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchFavoriteRecipes();
+  }
+
+  Future<void> fetchFavoriteRecipes() async {
+    DocumentReference userRef = FirebaseFirestore.instance
+        .collection('users')
+        .doc(FirebaseAuth.instance.currentUser!.uid);
+
+    QuerySnapshot favoritesSnapshot = await FirebaseFirestore.instance
+        .collection('favorites')
+        .where('userId', isEqualTo: userRef)
+        .get();
+
+    List favoriteRecipeIds =
+        favoritesSnapshot.docs.map((doc) => doc['recipeId'].id).toList();
+
+    List<Recipe> recipes = [];
+
+    for (String recipeId in favoriteRecipeIds) {
+      DocumentSnapshot recipeDoc = await FirebaseFirestore.instance
+          .collection('recipes')
+          .doc(recipeId)
+          .get();
+      if (recipeDoc.exists) {
+        Recipe recipe = await Recipe.fromFirestore(recipeDoc);
+        recipes.add(recipe);
+      }
+    }
+
+    setState(() {
+      favoriteRecipes = recipes;
+    });
+  }
+
+  Future<void> toggleFavoriteStatus(Recipe recipe) async {
+    String currentUserId = FirebaseAuth.instance.currentUser!.uid;
+
+    DocumentReference favoriteRef = FirebaseFirestore.instance
+        .collection('favorites')
+        .doc('$currentUserId${recipe.id}');
+    await favoriteRef.delete();
+    await fetchFavoriteRecipes();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -91,86 +120,123 @@ class FavoritePage extends StatelessWidget {
           ),
         ),
       ),
-      body: ListView.builder(
-        itemCount: favoriteRecipes.length,
-        itemBuilder: (context, index) {
-          final recipe = favoriteRecipes[index];
-          return Card(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(15.0),
-            ),
-            margin: EdgeInsets.all(10.0),
-            elevation: 5.0,
-            child: InkWell(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => RecipeDetailPage(recipe: recipe)),
-                );
-              },
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Hero(
-                    tag: recipe.imageUrl,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(15.0),
-                        topRight: Radius.circular(15.0),
-                      ),
-                      child: Image.network(
-                        recipe.imageUrl,
-                        height: 200,
-                        width: double.infinity,
-                        fit: BoxFit.cover,
-                      ),
-                    ),
+      body: favoriteRecipes.isEmpty
+          ? Center(
+              child: Text('No favorite recipes yet',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+            )
+          : ListView.builder(
+              itemCount: favoriteRecipes.length,
+              itemBuilder: (context, index) {
+                final recipe = favoriteRecipes[index];
+                return Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0),
+                  elevation: 5.0,
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) =>
+                                RecipeDetailPage(recipe: recipe)),
+                      );
+                    },
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              recipe.title,
-                              style: TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                              ),
+                        Hero(
+                          tag: recipe.imageUrl,
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(15.0),
+                              topRight: Radius.circular(15.0),
                             ),
-                            IconButton(
-                              icon: Icon(
-                                Icons.favorite,
-                                color: recipe.isFavorite ? Colors.red : Colors.grey,
-                              ),
-                              onPressed: () {
-                                // Handle favorite button press
+                            child: Image.network(
+                              recipe.imageUrl,
+                              height: 200,
+                              width: double.infinity,
+                              fit: BoxFit.cover,
+                              loadingBuilder:
+                                  (context, child, loadingProgress) {
+                                if (loadingProgress == null) return child;
+                                return Center(
+                                  child: CircularProgressIndicator(
+                                    value: loadingProgress.expectedTotalBytes !=
+                                            null
+                                        ? loadingProgress
+                                                .cumulativeBytesLoaded /
+                                            loadingProgress.expectedTotalBytes!
+                                        : null,
+                                  ),
+                                );
                               },
+                              errorBuilder: (context, error, stackTrace) =>
+                                  Center(
+                                child: Icon(
+                                  Icons.error,
+                                  color: Colors.red,
+                                ),
+                              ),
                             ),
-                          ],
+                          ),
                         ),
-                        SizedBox(height: 5),
-                        Text(
-                          recipe.description,
-                          style: TextStyle(fontSize: 14),
-                        ),
-                        SizedBox(height: 5),
-                        Text(
-                          'By ${recipe.creator}',
-                          style: TextStyle(fontSize: 14, fontStyle: FontStyle.italic),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Flexible(
+                                    child: Text(
+                                      recipe.title,
+                                      style: TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ),
+                                  IconButton(
+                                    icon: Icon(
+                                      Icons.favorite,
+                                      color: recipe.isFavorite
+                                          ? Colors.red
+                                          : Colors.grey,
+                                    ),
+                                    onPressed: () {
+                                      toggleFavoriteStatus(recipe);
+                                    },
+                                  ),
+                                ],
+                              ),
+                              SizedBox(height: 5),
+                              Text(
+                                recipe.description,
+                                style: TextStyle(fontSize: 14),
+                                maxLines: 2,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                              SizedBox(height: 5),
+                              Text(
+                                'By ${recipe.creator}',
+                                style: TextStyle(
+                                    fontSize: 14, fontStyle: FontStyle.italic),
+                              ),
+                            ],
+                          ),
                         ),
                       ],
                     ),
                   ),
-                ],
-              ),
+                );
+              },
             ),
-          );
-        },
-      ),
     );
   }
 }
